96efe075feb9b244b206895b998a0746b4642524
alxndr29 <alexevan2910@gmail.com>
2022-07-03 22:22:38 +0800

    role guard 403

---
+++ app/Http/Middleware/CekPegawai.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+class CekPegawai
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::user()->role == "pegawai"){
+            return $next($request);
+        }else{
+            abort(403, 'Unauthorized action.');
+        }
+       
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -21,24 +21,31 @@ Route::get('/jamserver', function () {
 })->name('waktuserver');
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
-//Admin
-Route::get('/admin', 'AdminController@index')->name('admin.index');
-Route::get('/pegawai', 'AdminController@pegawai')->name('admin.pegawai');
-Route::post('/pegawai/store', 'AdminController@storePegawai')->name('admin.storepegawai');
-Route::delete('/pegawai/destroy/{id}', 'AdminController@destroyPegawai')->name('admin.destroypegawai');
-Route::put('/pegawai/update/{id}', 'AdminController@updatePegawai')->name('admin.updatepegawai');
-Route::get('/periode', 'AdminController@periodePresensi')->name('admin.periodepresensi');
-Route::get('/periode/detail/{id}', 'AdminController@detailPeriodePresensi')->name('admin.periodedetail');
-Route::post('/periode/store', 'AdminController@storePeriodePresensi')->name('admin.storeperiode');
-Route::get('/laporan/{start?}/{end?}', 'AdminController@laporan')->name('admin.laporan');
-Route::put('/presensi/update/{iduser}/{idperiode}', 'AdminController@updateDataPresensi')->name('admin.ubahdatapresensi');
-Route::get('/presensi/edit/{iduser}/{idperiode}', 'AdminController@editDataPresensi')->name('admin.editdatapresensi');
-Route::get('/detail/pegawai/{id}/{start?}/{end?}', 'AdminController@laporanDetailKariawan')->name('admin.detailpegawai');
-Route::post("/resetnotifikasi/{id}", 'AdminController@resetNotifikasiTidakHadir')->name('admin.resetnotifikasi');
-Route::get("/lokasi", 'AdminController@indexLokasi')->name('admin.lokasi');
-Route::put("/lokasi/update", 'AdminController@updateLokasi')->name('admin.lokasiupdate');
-//Pegawai
-Route::get('/pegawai/home', 'PegawaiController@index')->name('pegawai.index');
-Route::get('/pegawai/presensimasuk', 'PegawaiController@presensiMasuk')->name('pegawai.presensimasuk');
-Route::get('/pegawai/presensikeluar', 'PegawaiController@presensiKeluar')->name('pegawai.presensikeluar');
-Route::get('/pegawai/laporan/{start?}/{end?}', 'PegawaiController@laporan')->name('pegawai.laporan');
+
+Route::group(['middleware' => ['auth','cekadmin']],function(){
+    //Admin
+    Route::get('/admin', 'AdminController@index')->name('admin.index');
+    Route::get('/pegawai', 'AdminController@pegawai')->name('admin.pegawai');
+    Route::post('/pegawai/store', 'AdminController@storePegawai')->name('admin.storepegawai');
+    Route::delete('/pegawai/destroy/{id}', 'AdminController@destroyPegawai')->name('admin.destroypegawai');
+    Route::put('/pegawai/update/{id}', 'AdminController@updatePegawai')->name('admin.updatepegawai');
+    Route::get('/periode', 'AdminController@periodePresensi')->name('admin.periodepresensi');
+    Route::get('/periode/detail/{id}', 'AdminController@detailPeriodePresensi')->name('admin.periodedetail');
+    Route::post('/periode/store', 'AdminController@storePeriodePresensi')->name('admin.storeperiode');
+    Route::get('/laporan/{start?}/{end?}', 'AdminController@laporan')->name('admin.laporan');
+    Route::put('/presensi/update/{iduser}/{idperiode}', 'AdminController@updateDataPresensi')->name('admin.ubahdatapresensi');
+    Route::get('/presensi/edit/{iduser}/{idperiode}', 'AdminController@editDataPresensi')->name('admin.editdatapresensi');
+    Route::get('/detail/pegawai/{id}/{start?}/{end?}', 'AdminController@laporanDetailKariawan')->name('admin.detailpegawai');
+    Route::post("/resetnotifikasi/{id}", 'AdminController@resetNotifikasiTidakHadir')->name('admin.resetnotifikasi');
+    Route::get("/lokasi", 'AdminController@indexLokasi')->name('admin.lokasi');
+    Route::put("/lokasi/update", 'AdminController@updateLokasi')->name('admin.lokasiupdate');
+});
+Route::group(['middleware' => ['auth', 'cekpegawai']], function () {
+    //Pegawai
+    Route::get('/pegawai/home', 'PegawaiController@index')->name('pegawai.index');
+    Route::get('/pegawai/presensimasuk', 'PegawaiController@presensiMasuk')->name('pegawai.presensimasuk');
+    Route::get('/pegawai/presensikeluar', 'PegawaiController@presensiKeluar')->name('pegawai.presensikeluar');
+    Route::get('/pegawai/laporan/{start?}/{end?}', 'PegawaiController@laporan')->name('pegawai.laporan');
+});
+
+
